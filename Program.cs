64358079f0486c8cd91f60ea1cc387eaaf8dfa65
IB-315168 <315168@via.dk>
2022-10-09 20:42:32 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcGreeterClient;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        using var channel = GrpcChannel.ForAddress("http://localhost:8080");
+        var client = new HelloService.HelloServiceClient(channel);
+        var reply = await client.helloAsync(new HelloRequest() { FirstName = "Igor", LastName = "Bulinski"});
+        Console.WriteLine("Greeting: " + reply.Greeting);
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
